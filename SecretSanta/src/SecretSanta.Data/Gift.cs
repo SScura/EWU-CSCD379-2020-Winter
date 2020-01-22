4018216c9bbacc50311512076bf9ea536d812f99
Sean Scura <sean.scura@intellitect.com>
2020-01-22 11:17:28 -0800

    Changing properties back

--- SecretSanta/src/SecretSanta.Data/Gift.cs
+++ SecretSanta/src/SecretSanta.Data/Gift.cs
@@ -7,10 +7,11 @@ namespace SecretSanta.Data
         public string Title { get => _Title; set => _Title = value ?? throw new ArgumentNullException(nameof(Title)); }
         public string Description { get => _Description; set => _Description = value ?? throw new ArgumentNullException(nameof(Description)); }
         public string Url { get => _Url; set => _Url = value ?? throw new ArgumentNullException(nameof(Url)); }
-        public User User { get => _User; set; }
+#nullable disable
+        public User User { get ; set; }
+#nullable enable
         private string _Url = string.Empty;
         private string _Title = string.Empty;
         private string _Description = string.Empty;
-        private string _User = string.Empty;
     }
 }
--- SecretSanta/src/SecretSanta.Data/User.cs
+++ SecretSanta/src/SecretSanta.Data/User.cs
@@ -7,8 +7,8 @@ namespace SecretSanta.Data
     {
         public string FirstName { get => _FirstName; set => _FirstName = value ?? throw new ArgumentNullException(nameof(FirstName)); }
         public string LastName { get => _LastName; set => _LastName = value ?? throw new ArgumentNullException(nameof(LastName)); }
-        public User Santa { get; set; }
 #nullable disable
+        public User Santa { get; set; }
         public ICollection<Gift> Gifts { get; set; }
         public List<GroupInfo> GroupsInfo { get; set; }
 #nullable enable
