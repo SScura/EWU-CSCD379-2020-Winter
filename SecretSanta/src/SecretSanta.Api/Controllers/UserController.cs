639bf2ab4024f13c149d7fae051d501ba55d3762
Sean Scura <sean.scura@intellitect.com>
2020-02-14 05:25:09 -0800

    Set up User and Gift controller

--- SecretSanta/src/SecretSanta.Api/Controllers/UserController.cs
+++ SecretSanta/src/SecretSanta.Api/Controllers/UserController.cs
@@ -1,13 +1,78 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using SecretSanta.Business.Dto;
 using SecretSanta.Business.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SecretSanta.Api.Controllers
 {
-    public class UserController : BaseApiController<User, UserInput>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
+        private IUserService UserService { get; }
+
         public UserController(IUserService userService)
-            : base(userService)
-        { }
+        {
+            UserService = userService ?? throw new System.ArgumentNullException(nameof(userService));
+        }
+
+        // GET: https://localhost/api/User
+        [HttpGet]
+        public async Task<IEnumerable<User>> Get()
+        {
+            List<User> users = await UserService.FetchAllAsync();
+            return users;
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<User>> Get(int id)
+        {
+            if (await UserService.FetchByIdAsync(id) is User user)
+            {
+                return Ok(user);
+            }
+            return NotFound();
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public async Task<User> Post(UserInput value)
+        {
+
+            return await UserService.InsertAsync(value);
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<User>> Put(int id, UserInput value)
+        {
+            if (await UserService.UpdateAsync(id, value) is User user)
+            {
+                return user;
+            }
+            return NotFound();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (await UserService.DeleteAsync(id))
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
     }
 }
